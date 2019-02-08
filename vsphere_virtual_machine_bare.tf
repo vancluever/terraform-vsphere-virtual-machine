179053dadbbd3689aaa7c701fe557f5ed0f1922d
Koleon <Koleon@users.noreply.github.com>
2019-02-08 10:25:22 +0100

    Without the hyphen when var.vm_count == 1
    As Antoine98 mention [here](https://github.com/vancluever/terraform-vsphere-virtual-machine/pull/3#issuecomment-461627555)

--- vsphere_virtual_machine_bare.tf
+++ vsphere_virtual_machine_bare.tf
@@ -1,6 +1,6 @@
 resource "vsphere_virtual_machine" "virtual_machine_bare" {
   count            = "${var.template_os_family == "" ? var.vm_count : 0}"
-  name             = "${var.vm_name_prefix}-${var.vm_count == 1 ? "" : "${count.index}"}"
+  name             = "${var.vm_name_prefix}${var.vm_count == 1 ? "" : "-${count.index}"}"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.ds.id}"
 
